faa4231dd27a3c88a0f4fddaf52dc351f565f336
chengangcode <chengangxz@163.com>
2020-05-19 13:09:26 +0800

    修改关于jenkins运行时路径报错问题

--- run_test.py
+++ run_test.py
@@ -1,11 +1,14 @@
 import time,sys
-sys.path.append('./test_case')
+#sys.path.append('./test_case')
 from HTMLTestRunner import HTMLTestRunner
 import unittest
 from lib.send_mail import Send_Mail
+import os
 
 #指定测试用例为当前文件下的interface目录
-test_dir = './test_case'
+path = os.path.split(os.path.realpath(__file__))[0]
+#print(path)
+test_dir = os.path.join(path,'test_case')
 discover = unittest.defaultTestLoader.discover(test_dir,pattern='*_test.py')
 send = Send_Mail()
 
@@ -15,7 +18,8 @@ if __name__ == '__main__':
     #now = datetime.datetime.now().strftime("%Y-%m-%d  %H:%M:%S")
     #now = time.strftime("%Y-%m-%d #H:%M:%S", time.localtime(time.time()))  # 获取系统时间
     now = time.strftime('%Y-%m-%d %H.%M.%S',time.localtime(time.time()))
-    filename = './report/' + now + '_result.html'
+    filename = 'D:/pycharm/project/直连天下后台管理系统/report/' + now + '_result.html'
+    print(filename)
     fp = open(filename,'wb')
     runner = HTMLTestRunner(stream = fp,
                             title = '直连天下系统接口自动化测试报告',
--- test_case/test111.py
+++
@@ -1,9 +0,0 @@
-import pymysql
-from sshtunnel import SSHTunnelForwarder
-
-class ExecuteSql(object):
-    def __init__(self,dbname,sql):
-        self.dbname = dbname
-        self.sql = sql
-
-    def excute_
